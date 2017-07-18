7a853a43aa661a2f0fdbd9785be762483dfe2ee6
summerwaves <summerwaves@126.com>
2017-07-18 20:32:50 +0800

    mybatis使用xml和注释两种方法操作数据库

--- src/main/java/cn/summerwaves/dao/UserDao.java
+++ src/main/java/cn/summerwaves/dao/UserDao.java
@@ -1,8 +1,13 @@
 package cn.summerwaves.dao;
 
+import org.apache.ibatis.annotations.Insert;
+
 /**
  * Created by summerwaves on 2017/7/18.
  */
 public interface UserDao {
     void insertUser();
+
+    @Insert("INSERT INTO user(user_name,password,sex)VALUE (#{userName},#{password},#{sex});")
+    void insertUserByAnnotation(String userName,String password,int sex);
 }
--- src/test/java/cn/summerwaves/dao/UserDaoTest.java
+++ src/test/java/cn/summerwaves/dao/UserDaoTest.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,28 +17,44 @@ import org.junit.Test;
  */
 public class UserDaoTest {
     private SqlSessionFactory factory;
+    private SqlSession session;
 
 
     @Before
     public void setUp() throws Exception {
         String resource = "mybatis-config.xml";
         factory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(resource));
+        session = factory.openSession();
     }
 
 
 
     @Test
-    public void insertUser() throws Exception {
-        SqlSession session = factory.openSession();
+    public void insertUserByXML() throws Exception {
         User user = new User();
         user.setUserName("username");
         user.setPassword("password");
         user.setSex(1);
-        session.insert("cn.summerwaves.mapper.UserDao.insertUser", user);
+        session.insert("cn.summerwaves.dao.UserDao.insertUser", user);
+        System.out.println("add user by XML");
+    }
+
+    @Test
+    public void insertUserByAnnotation() throws Exception {
+        User user = new User();
+        user.setUserName("username");
+        user.setPassword("password");
+        user.setSex(1);
+        session.insert("cn.summerwaves.dao.UserDao.insertUserByAnnotation", user);
+        System.out.println("add user by annotation");
+    }
+
+    @After
+    public void teardown() throws Exception {
         session.commit();
         session.close();
-        System.out.println("add user");
 
     }
+
 
 }
